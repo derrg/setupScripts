9e651ea64d0274c1c37ff11b5bf4448b1ffc50d5
Ken Van Dalsum <kenvandalsum@gmail.com>
2019-03-31 17:33:38 -0400

    Updated Chocolatey install script

--- windows/installChocolatey.ps1
+++ windows/installChocolatey.ps1
@@ -1,20 +1,24 @@
-# Install Chocolatey
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if (-Not(Get-Command "choco" -ErrorAction SilentlyContinue)) {
+    # Install Chocolatey
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Reload PATH
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    # Reload PATH
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
 
 # Array of programs to install
 [string[]] $programs = @(
-                "awscli", "ccleaner", "cmake", "cmder", "docker", "dotnet4.7",
-                "everything", "git", "git-lfs.install", "gradle", "jdk8", "poshgit",
-                "procexp", "python", "r.project", "speccy", "virtualbox", "vlc",
-                "visualstudiocode", "windirstat", "winmerge"
-            )
-
+    "7zip.install", "adobereader", "audacity", "awscli", "blender", "ccleaner", "cmder", "cpu-z", "cue", "deluge",
+    "dep", "docker", "dotnet4.7", "everything", "firefox", "gifcam", "gimp", "git", "git-lfs", "golang",
+    "googlechrome", "gradle", "hexchat", "inkscape", "jdk11", "jdk8", "jetbrainstoolbox", "k-litecodecpackfull", "kindle", "krita",
+    "libreoffice", "lmms", "mingw", "obs-studio", "openjdk", "openjdk11", "openssh", "origin", "poshgit", "postman",
+    "procexp", "python", "quicklook", "r.project", "sharex", "signal", "speccy", "spotify", "steam", "sysinternals",
+    "telegram", "teracopy", "terraform", "thunderbird", "unity", "visualstudiocode", "vlc", "vscode", "windirstat", "winmerge",
+    "winscp", "wireshark"
+)
 # Install packages for development
 foreach ($program in $programs) {
     Write-Host "Attempting to install $program..."
-    choco install $program -y
+    choco.exe install $program -y
     Write-Host "End attempt to install $program..."
 }
