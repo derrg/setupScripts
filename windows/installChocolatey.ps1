48f9bf15f348d1579e8aaab1c65497ba81d45c56
Ken Van Dalsum <ken@kenvandalsum.com>
2020-04-25 23:12:31 -0400

    Updated Chocolatey packages

--- windows/installChocolatey.ps1
+++ windows/installChocolatey.ps1
@@ -8,13 +8,13 @@ if (-Not(Get-Command "choco" -ErrorAction SilentlyContinue)) {
 
 # Array of programs to install
 [string[]] $programs = @(
-    "7zip.install", "adobereader", "audacity", "awscli", "blender", "ccleaner", "cmder", "cpu-z", "cue", "deluge",
-    "dep", "docker", "dotnet4.7", "everything", "firefox", "gifcam", "gimp", "git", "git-lfs", "golang",
-    "googlechrome", "gradle", "hexchat", "inkscape", "jdk11", "jdk8", "jetbrainstoolbox", "k-litecodecpackfull", "kindle", "krita",
-    "libreoffice", "lmms", "mingw", "obs-studio", "openjdk", "openjdk11", "openssh", "origin", "poshgit", "postman",
-    "procexp", "python", "quicklook", "r.project", "sharex", "signal", "speccy", "spotify", "steam", "sysinternals",
-    "telegram", "teracopy", "terraform", "thunderbird", "unity", "visualstudiocode", "vlc", "vscode", "windirstat", "winmerge",
-    "winscp", "wireshark"
+    "7zip.install", "adobereader", "audacity", "awscli", "blender", "ccleaner", "cpu-z", "cue", "deluge", "dep",
+    "docker", "dotnet4.7", "everything", "firefox", "gifcam", "gimp", "git", "git-lfs", "golang", "googlechrome",
+    "gradle", "groovy", "itunes", "jdk11", "jetbrainstoolbox", "k-litecodecpackfull", "kindle", "krita", "kubernetes-cli", "kubernetes-helm"
+    "libreoffice", "lmms", "microsoft-windows-terminal", "mingw", "openjdk", "openjdk11", "openssh", "origin",
+    "procexp", "python", "quicklook", "r.project", "sharex", "signal", "speccy", "spotify", "steam", "streamlabs-obs", "sysinternals",
+    "telegram", "terraform", "thunderbird", "unity", "visualstudiocode", "vlc", "vscode", "windirstat", "winmerge",
+    "winscp", "wireshark", "xmlstartlet"
 )
 # Install packages for development
 foreach ($program in $programs) {
