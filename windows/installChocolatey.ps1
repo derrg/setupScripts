b4d991712e6c0fe8929115e269de745c6cfae809
Ken Van Dalsum <ken@kenvandalsum.com>
2020-05-16 10:09:14 -0400

    Removed Piriform products and replaced them with reputable alternatives

--- windows/installChocolatey.ps1
+++ windows/installChocolatey.ps1
@@ -8,11 +8,11 @@ if (-Not(Get-Command "choco" -ErrorAction SilentlyContinue)) {
 
 # Array of programs to install
 [string[]] $programs = @(
-    "7zip.install", "adobereader", "audacity", "awscli", "blender", "ccleaner", "cpu-z", "cue", "deluge", "dep",
+    "7zip.install", "adobereader", "audacity", "awscli", "bleachbit", "blender", "cpu-z", "cue", "deluge", "dep",
     "docker", "dotnet4.7", "everything", "firefox", "gifcam", "gimp", "git", "git-lfs", "golang", "googlechrome",
-    "gradle", "groovy", "itunes", "jdk11", "jetbrainstoolbox", "k-litecodecpackfull", "kindle", "krita", "kubernetes-cli", "kubernetes-helm"
+    "gradle", "groovy", "hwinfo", "itunes", "jdk11", "jetbrainstoolbox", "k-litecodecpackfull", "kindle", "krita", "kubernetes-cli", "kubernetes-helm"
     "libreoffice", "lmms", "microsoft-windows-terminal", "mingw", "openjdk", "openjdk11", "openssh", "origin",
-    "procexp", "python", "quicklook", "r.project", "sharex", "signal", "speccy", "spotify", "steam", "streamlabs-obs", "sysinternals",
+    "procexp", "python", "quicklook", "r.project", "sharex", "signal", "spotify", "steam", "streamlabs-obs", "sysinternals",
     "telegram", "terraform", "thunderbird", "unity", "visualstudiocode", "vlc", "vscode", "windirstat", "winmerge",
     "winscp", "wireshark", "xmlstartlet"
 )
